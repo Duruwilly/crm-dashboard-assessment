68aef9b23165ff544622afe1fb37f756a6ee14f8
Princewill Duru <duruprincewilluzochukwu@gmail.com>
2025-12-07 18:02:22 +0100

    fix: adjust padding and visibility for layout components in mobile responsiveness

--- app/routes/auth/layout.tsx
+++ app/routes/auth/layout.tsx
@@ -33,14 +33,14 @@ const AuthLayout = () => {
 
   return (
     <div className="w-full h-screen flex lg:flex-row flex-col">
-      <div className="w-full lg:max-w-[50%] h-screen bg-white z-20 px-[50px] py-20 overflow-y-auto">
+      <div className="w-full lg:max-w-[50%] h-screen bg-white z-20 px-5 sm:px-[50px] py-20 overflow-y-auto">
         <div className="w-[90%] max-w[342px] mx-auto mb-8 lg:mb-24 flex flex-col">
           {/*  */}
           <Outlet />
         </div>
       </div>
 
-      <div className="w-full lg:max-w-[50%] p-2 hscreen relative bg-black-400">
+      <div className="w-full lg:max-w-[50%] p-2 hscreen relative bg-black-400 hidden lg:flex">
         {/*  */}
       </div>
     </div>
